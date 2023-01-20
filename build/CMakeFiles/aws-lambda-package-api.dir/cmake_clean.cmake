d30faa94fca6e1f92a4047bd8983a7e7204275eb
Venilton FalvoJr <falvojr@gmail.com>
2023-01-19 22:34:56 -0300

    AWS C++ Lambda working \o/

--- build/CMakeFiles/aws-lambda-package-api.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/aws-lambda-package-api"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/aws-lambda-package-api.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
